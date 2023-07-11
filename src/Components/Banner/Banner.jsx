47a674d49e045504cb45bb2479087a2826311f5a
Tom Xavier <tomxavieronthego@gmail.com>
2023-07-11 12:16:23 +0530

    First commit

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <div className='content'>
+                <h1 className='title'>Movie Name</h1>
+                <div className='banner-button'>
+                    <button className='button'>Play</button>
+                    <button className='button'>My List</button>
+
+                </div>
+                <h4 className='description'> ewagzhfxgnhrSHxfhhtrmhhrhzgmfhrznmh
+                    ryhgnrtyrh 5etr rtsyh aerhsy eydhf srh
+                    fs gsr ryhjmd yezrdhfwyw sy4w5yw y4wyhwh srywshtb
+                </h4>
+            </div>
+            <div className="fade_bottom"></div>
+        </div>
+    );
+};
+
+export default Banner;
